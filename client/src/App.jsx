8d77bd8c37a6b6300b8c4014ca159e8f21f392eb
Tembanator <49640058+Tembanator@users.noreply.github.com>
2023-10-09 19:46:12 +0200

    complete sign up page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route, Router } from 'react-router-dom'
-import Home from './assets/Home'
-import SignUp from './assets/SignUp'
-import Profile from './assets/Profile'
-import About from './assets/about'
-import SignIn from './assets/Signin'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/about'
+import SignIn from './pages/Signin'
 import Header from './components/Header'
 export default function App() {
   return <BrowserRouter>
